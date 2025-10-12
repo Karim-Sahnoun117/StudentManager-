1abe47ee282fa5697944ad2109004423f91c5e27
Karim-Sahnoun117 <karimsahnoun12@gmail.com>
2025-10-12 16:56:35 +0100

    create database config file

---
+++ database.py
@@ -0,0 +1,9 @@
+import psycopg2 as p6
+
+
+class database:
+    def __init__(self):
+        self.conn = p6.connect(database= "hello",user= "postgres",password = "secret",host= "localhost",port = 5432)
+        self.curr = self.conn.cursor()
+
+    
